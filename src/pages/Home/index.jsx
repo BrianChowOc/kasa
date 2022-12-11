d483c2df2f3af81c61d000f59d72360f01caa9ce
cbrian <brianchow26@gmail.com>
2022-12-11 21:12:34 +0100

    feat: added home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,35 @@
+import styled from "styled-components";
+import Banner from "../../components/Banner";
+import Card from "../../components/Card";
+
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  background-color: #f6f6f6;
+  border-radius: 25px;
+  margin-top: 43px;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 26px;
+  padding-bottom: 13px;
+`;
+
 function Home() {
-  return <h1>Page d'accueil</h1>;
+  return (
+    <>
+      <Banner />
+      <CardContainer>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </CardContainer>
+    </>
+  );
 }
 
 export default Home;
